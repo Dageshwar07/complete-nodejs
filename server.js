1b0513f3506e8ca2662038c229c5864df2b44f3b
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-10-18 00:07:45 +0530

    file create through input

--- server.js
+++ server.js
@@ -12,4 +12,15 @@ http.createServer(function (req, res) {
 console.log("first")
 
 const input = process.argv
-fs.writeFileSync(input[2],input[3])
+if (input[2] == 'add') {
+
+    fs.writeFileSync(input[3], input[4])
+
+}
+else if (input[2] == 'remove') {
+    fs.unlinkSync(input[3])
+
+}
+else {
+    console.log("invalid output")
+}
